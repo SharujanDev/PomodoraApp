20ac84f289f163b545714d126dd7a5345377998d
SharujanDev <74742962+SharujanDev@users.noreply.github.com>
2024-07-18 15:48:04 +0100

    first commit

---
+++ src/pomodoraApp.java
@@ -0,0 +1,23 @@
+import java.sql.Time;
+
+import javafx.application.*;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import
+class pomodoraApp extends Application
+{ 
+    Label timer;
+    int time = 600;
+
+    public void start(Stage primaryStage)
+    {
+        Time time = new Time(600);
+
+
+    }
+
+    public static void main(String[] args) {launch(args);}
+
+}
+
+
